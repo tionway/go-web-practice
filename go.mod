53fb02b8761ba74aa4101470570109be58315ce8
tionway <96904564+tionway@users.noreply.github.com>
2024-05-19 14:16:53 +0800

    database

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tionway/go-web-practice
 
 go 1.22.2
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
-	"net/http"
+	"log"
+	// "net/http"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+const (
+	user      = "shen"
+	password  = ""
+	port      = "localhost:5432"
+	dbname    = "postgres"
+	parameter = "sslmode=disable"
 )
 
 func main() {
-	server := http.Server{
-		Addr:    "localhost:8080",
-		Handler: nil,
+	connStr := fmt.Sprintf("postgres://%s:%s@%s/%s?%s", user, password, port, dbname, parameter)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalln(err.Error())
 	}
-	fmt.Println("Hello World!")
-	server.ListenAndServe()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	fmt.Println("Connected!")
 }
